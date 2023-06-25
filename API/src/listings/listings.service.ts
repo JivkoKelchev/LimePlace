653f04d875f188fe8736181a1f93443789bd7c94
JivkoKelchev <jivko.r.kelchev@gmail.com>
2023-06-25 16:51:11 +0300

    Added sort by price and filter by user in API

--- API/src/listings/listings.service.ts
+++ API/src/listings/listings.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import {Listing} from "./listing.entity";
 import {InjectRepository} from "@nestjs/typeorm";
-import {ListDatabasesResult, Repository} from "typeorm";
+import {FindOptionsOrder, ListDatabasesResult, Repository} from "typeorm";
 
 @Injectable()
 export class ListingsService {
@@ -20,13 +20,28 @@ export class ListingsService {
     // }
 
     async findAllActive(query?): Promise<{data:Listing[], count:number}> {
+        //BuildQuery
+        let whereClauseObject : { active: boolean, owner?: string} = { active: true }
+        if(query.user) {
+            whereClauseObject.owner = query.user;
+        }
+        let orderByObject : FindOptionsOrder<Listing>
+        if(query.sort) {
+            orderByObject = { price : 'ASC' }
+        } else {
+            orderByObject = { updated_at : "DESC" }
+        }
+        //pagination
         const page = query?.page || 1;
         const take = 5;
         const skip = (page - 1) * take;
+        //user
+        
+        let userQuery : { user: string };
 
         const [result, total] = await this.listingRepository.findAndCount(
             {
-                where: { active: true }, order: { updated_at: "DESC" },
+                where: whereClauseObject, order: orderByObject,
                 take: take,
                 skip: skip
             }
