e2bc8f9229630568517825017e06004d76b9de38
JivkoKelchev <jivko.r.kelchev@gmail.com>
2023-06-26 16:14:51 +0300

    added init sdk using metamask-sdk

--- CLI/controllers/connectionController.ts
+++ CLI/controllers/connectionController.ts
@@ -2,6 +2,8 @@ import {networkList, networkMenu} from "../views/menu/connection/networksPrompt"
 import {LocalNetworkData, localNetworkPrompts} from "../views/menu/connection/localContractsPrompt";
 import {ethers} from "ethers";
 import {Sdk} from "../services/sdk";
+// @ts-ignore
+import {MetaMaskSDK} from "@metamask/sdk";
 
 let sdkInstance: Sdk | null = null;
 let instanceCreated = false;
@@ -22,16 +24,9 @@ const connect = async (network: string) : Promise<Sdk> => {
             //const data = await localNetworkPrompts();
             return await initSdkForLocalNetwork();
         }
-        case networkList[1]: {
-            //todo add metamask sdk
-            console.log('not implemented')
-            // const data = await localNetworkPrompts();
-            return await initSdkForLocalNetwork();
-        }
+        case networkList[1]: 
         default: {
-            console.log('not implemented')
-            // const data = await localNetworkPrompts();
-            return await initSdkForLocalNetwork();
+            return await initSdk();
         }
     }
     
@@ -45,6 +40,19 @@ const selectNetwork = async () : Promise<string> => {
 const initSdkForLocalNetwork = async  () : Promise<Sdk> => {
     const provider = new ethers.JsonRpcProvider("http://127.0.0.1:8545/");
     const signer = await provider.getSigner(`${process.env.LOC_USER_WALLET_ADR}`);
+    sdkInstance = new Sdk(provider, `${process.env.LOC_LIME_PLACE_ADR}`, `${process.env.LOC_LIME_PLACE_NFT_ADR}`, signer);
+    instanceCreated = true;
+    return sdkInstance;
+}
+
+const initSdk = async () : Promise<Sdk> => {
+    const MMSDK = new MetaMaskSDK({dappMetadata : {name: "My Dapp"}});
+    const ethereum = await MMSDK.getProvider();
+    // @ts-ignore
+    await ethereum.request({ method: 'eth_requestAccounts', params: [] });
+
+    const provider = new ethers.JsonRpcProvider(ethereum);
+    const signer = await provider.getSigner();
     sdkInstance = new Sdk(provider, `${process.env.LOC_LIME_PLACE_ADR}`, `${process.env.LOC_LIME_PLACE_NFT_ADR}`, signer);
     instanceCreated = true;
     return sdkInstance;
