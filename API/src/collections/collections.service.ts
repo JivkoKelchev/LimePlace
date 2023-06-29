794e548cc5f25ec4afdf720815dbf93f4d721ad4
JivkoKelchev <jivko.r.kelchev@gmail.com>
2023-06-29 14:26:18 +0300

    update collections volume selection

--- API/src/collections/collections.service.ts
+++ API/src/collections/collections.service.ts
@@ -3,6 +3,7 @@ import {Collection} from "./collections.enitity";
 import {InjectRepository} from "@nestjs/typeorm";
 import {Repository} from "typeorm";
 import {Listing} from "../listings/listing.entity";
+import {ListingHistory} from "../listingsHistory/listingHistory.entity";
 
 @Injectable()
 export class CollectionsService {
@@ -22,16 +23,20 @@ export class CollectionsService {
         select('coll.name', 'name').
         addSelect('coll.owner', 'owner').
         addSelect('MIN(listing.price)', 'floor').
-        addSelect('volume.amount', 'volume').    
-        innerJoin('listing', 'listing', 'coll.address = listing.collection').
-            innerJoin(qb => {
-            return qb.subQuery()
-                .select("SUM(volTable.price)", 'amount')
-                .addSelect('volTable.collection', 'collection')
-                .from(Listing, "volTable")
-                .where("volTable.updated_at > :timestamp",{ timestamp: 1687954040 })
-                .groupBy('volTable.collection')
+        addSelect('volume.amount', 'volume').
+        //volume temp table    
+        leftJoin(qb => {
+        return qb.subQuery()
+            .select("SUM(history.price)", 'amount')
+            .addSelect('list.collection', 'collection')
+            .from(ListingHistory, "history")
+            //join listing to map: history->listing->collection
+            .leftJoin('listing', 'list', 'list.listingUid = history.listingUid')
+            .where("history.historyEvent='SOLD'")
+            .groupBy('list.collection')
         }, 'volume', 'volume.collection = coll.address').
+        //join listing to select only collections with active listings        
+        innerJoin('listing', 'listing', 'coll.address = listing.collection').
         where('listing.active = 1').
         groupBy('coll.name, coll.owner, volume.amount').
         getRawMany();
