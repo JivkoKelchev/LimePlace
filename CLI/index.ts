ba06ecc1789bf99464a49078bf3ddfa36e52b290
JivkoKelchev <jivko.r.kelchev@gmail.com>
2023-07-09 18:18:40 +0300

    catch api connection errors

--- CLI/index.ts
+++ CLI/index.ts
@@ -2,6 +2,8 @@ import {homeAction} from "./src/controllers/homeController";
 import {getSdk} from "./src/controllers/connectionController";
 import {config} from "dotenv";
 import {loadHeader} from "./src/views/header";
+import chalk from "chalk";
+import {AxiosError} from "axios";
 
 const run = async () => {
     try {
@@ -13,6 +15,12 @@ const run = async () => {
         //load main menu
         await homeAction();
     }catch (err) {
+        if(err instanceof AxiosError) {
+            if(err.code === 'ECONNRESET' || err.code === 'ECONNREFUSED') {
+                console.log(chalk.red('Api connection error...'));
+                process.exit(69);
+            }
+        }
         console.log(err);
         process.exit(69)
     }
