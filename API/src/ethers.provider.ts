51f77da61a34f36e04a52e9a8d0ce51e096accec
JivkoKelchev <jivko.r.kelchev@gmail.com>
2023-07-09 15:59:16 +0300

    add infura key into .env

--- API/src/ethers.provider.ts
+++ API/src/ethers.provider.ts
@@ -3,7 +3,7 @@ import {ConfigService} from "@nestjs/config";
 
 export const getProvider = (configService: ConfigService) => {
     //local provider
-    const providerUrl = configService.get<string>('NETWORK_URL');
+    const providerUrl = configService.get<string>('NETWORK_URL') + configService.get<string>('INFURA_KEY');
     return  new ethers.JsonRpcProvider(providerUrl);
     
 };
